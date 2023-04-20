8bdff2a290b2d46d3fa0b7c2f82dea4cab0d3e6f
Ray Wu <pickupfaith@gmail.com>
2023-04-21 02:57:24 +0800

    revise6

--- app/src/main/java/com/example/menuorder/ui/home/MenuOrderApp.kt
+++ app/src/main/java/com/example/menuorder/ui/home/MenuOrderApp.kt
@@ -25,12 +25,14 @@ import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -407,7 +409,7 @@ fun MealOrderCard(
         Text(
             style = MaterialTheme.typography.bodySmall,
             text = LocalContext.current.getString(meal.meal),
-            fontSize = 30.sp
+            fontSize = 25.sp
         )
         var imageResource by remember { mutableStateOf(R.drawable.cake) }
         imageResource = when (stringResource(meal.meal)) {
